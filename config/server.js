aa417bd627a6ed3c6e8317b38e442217a283e178
Jeziyah <17112273@student.hhs.nl>
2024-02-06 10:00:40 +0100

    strapi running

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
+"use strict";
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env('APP_KEYS'),
+    keys: env.array('APP_KEYS', 'JWT_SECRET'),
+    // keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
